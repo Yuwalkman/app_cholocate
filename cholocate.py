777c08bde9d8f2d4c024e42c085c5004123b6b58
Y Yao <yycwalkman@yahoo.com>
2025-05-12 19:49:02 +0800

    add the slide bar and humidy

--- cholocate.py
+++ cholocate.py
@@ -4,7 +4,11 @@ import datetime
 
 class BakingInfo:
     time_info = ft.TextField(label = "Time") 
-    tempeature_info = ft.TextField(label = " Temperature )", suffix_text="\u2103")
+    tempeature_info = ft.TextField(label = "Temperature", suffix_text="\u2103")
+    note_info = ft.TextField (label="Note",multiline=True,
+        hint_text = """please add your flavour info and your fragance you get in your baking
+
+        """)
 
 def main(page: ft.Page):
     
@@ -17,10 +21,12 @@ def main(page: ft.Page):
     page.scroll = ft.ScrollMode.AUTO  # 允许滚动
 
     t = ft.Text()
-    tb1 = ft.TextField(label = "Roaster Name ")
-    tb2 = ft.TextField(label = " Room Temperature )", suffix_text="\u2103")
+    tb1 = ft.TextField(label = "Roaster Name")
+    tb2 = ft.TextField(label = "Room Temperature", suffix_text="\u2103")
     tb3 = ft.TextField(label = "Beans Origin ")
-    tb4 = ft.TextField(label = " Date ",read_only=True)
+    tb4 = ft.TextField(label = "Date",read_only=True)
+    tb5 = ft.TextField(label= "weight", suffix_text="g")
+    tb6 = ft.TextField(label= "humidity", suffix_text="%")
     
     def check_info(e):
         ...
@@ -55,7 +61,7 @@ def main(page: ft.Page):
                     )
                 ),
             ),
-                        tb4,tb1,tb2,tb3,
+                        tb4,tb1,tb2,tb3,tb5,tb6,
                         ft.ElevatedButton("Next step", on_click=lambda _: page.go("/Example One")),
                         ft.AppBar(title=ft.Text("Roaster Info check "), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
                     ],
@@ -65,37 +71,119 @@ def main(page: ft.Page):
             page.views.append(
                 ft.View(
                     "/Example One",
-                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
                         ft.AppBar(title=ft.Text("Example One "), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
                         ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
                         ft.ElevatedButton("Next Example ", on_click=lambda _: page.go("/Example Two")),
                     ],
+                scroll=ft.ScrollMode.AUTO    
                 )
             )
+            
        
         elif page.route == "/Example Two":
             page.views.append(
                 ft.View(
                     "/Example Two",
-                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
                         ft.AppBar(title=ft.Text("Example Two "), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
                         ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
                         ft.ElevatedButton("Next Example ", on_click=lambda _: page.go("/Example Three")),
                         ft.ElevatedButton("previous Example ", on_click=lambda _: page.go("/Example One")),
                     ],
+                scroll=ft.ScrollMode.AUTO    
                 )
             )
         
         elif page.route == "/Example Three":
             page.views.append(
                 ft.View(
-                    "/Example Two",
-                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,
+                    "/Example Three",
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
                         ft.AppBar(title=ft.Text("Example Three "), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
                         ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
                         ft.ElevatedButton("Next Example ", on_click=lambda _: page.go("/Example Four")),
+                        ft.ElevatedButton("previous Example ", on_click=lambda _: page.go("/Example Two")),
+                    ],
+                scroll=ft.ScrollMode.AUTO    
+                )
+            )
+
+        elif page.route == "/Example Four":
+            page.views.append(
+                ft.View(
+                    "/Example Four",
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
+                        ft.AppBar(title=ft.Text("Example Four"), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.ElevatedButton("Next Example ", on_click=lambda _: page.go("/Example Five")),
                         ft.ElevatedButton("previous Example ", on_click=lambda _: page.go("/Example Three")),
                     ],
+                scroll=ft.ScrollMode.AUTO    
+                )
+            )
+        
+        elif page.route == "/Example Five":
+            page.views.append(
+                ft.View(
+                    "/Example Five",
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
+                        ft.AppBar(title=ft.Text("Example Five"), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.ElevatedButton("Next Example ", on_click=lambda _: page.go("/Example Six")),
+                        ft.ElevatedButton("previous Example ", on_click=lambda _: page.go("/Example Four")),
+                    ],
+                scroll=ft.ScrollMode.AUTO    
+                )
+            )
+
+        elif page.route == "/Example Six":
+            page.views.append(
+                ft.View(
+                    "/Example Five",
+                    [   bakingdeatil.time_info, bakingdeatil.tempeature_info,bakingdeatil.note_info,
+                        ft.AppBar(title=ft.Text("Example Five"), bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST),
+                        ft.Text("Sour:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Bitterr:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.Text("Dry:"),
+                        ft.Slider(min=0, max=5, divisions=5, label="{value}"),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                        ft.ElevatedButton("previous Example ", on_click=lambda _: page.go("/Example Five")),
+                        ft.ElevatedButton("Save", on_click=lambda _: page.go("/")),
+                    ],
+                scroll=ft.ScrollMode.AUTO    
                 )
             )
 
